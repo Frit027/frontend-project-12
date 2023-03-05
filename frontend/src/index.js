3abb5e4cd8bc7dc8e18bc386f2b1f01ba1da2f51
Frit027 <Frit_027@mail.ru>
2023-03-05 19:49:16 +0300

    added bootstrap css

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client';
 import init from './init';
-import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const app = async () => {
   const root = ReactDOM.createRoot(document.getElementById('root'));

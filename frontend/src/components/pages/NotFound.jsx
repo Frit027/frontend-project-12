6feff3dd20b044513e4e2b7c1cec9cad63a257d5
Frit027 <Frit_027@mail.ru>
2023-03-05 21:10:58 +0300

    added text and link

--- frontend/src/components/pages/NotFound.jsx
+++ frontend/src/components/pages/NotFound.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const NotFound = () => <div>Not found</div>;
+const NotFound = () => (
+  <div className="text-center">
+    <h1 className="h4 text-muted">Страница не найдена</h1>
+    <p className="text-muted">
+      Но вы можете перейти
+      <Link to="login"> на главную страницу</Link>
+    </p>
+  </div>
+);
 
 export default NotFound;

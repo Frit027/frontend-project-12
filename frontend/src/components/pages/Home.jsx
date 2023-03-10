cf8a392bb68891e88f61151412b16868887264a5
Frit027 <Frit_027@mail.ru>
2023-03-10 13:49:53 +0300

    check token instead context

--- frontend/src/components/pages/Home.jsx
+++ frontend/src/components/pages/Home.jsx
@@ -1,13 +1,6 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { Navigate } from 'react-router-dom';
-import UserContext from '../../AuthContext';
 
-const Home = () => {
-  const { loggedIn } = useContext(UserContext);
-
-  return (
-    loggedIn ? <h1>Home</h1> : <Navigate to="/login" />
-  );
-};
+const Home = () => (localStorage.getItem('token') ? <h1>Home</h1> : <Navigate to="/login" />);
 
 export default Home;

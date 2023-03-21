611f67120df99493e8f82e117998d1d14de85fa4
Frit027 <Frit_027@mail.ru>
2023-03-21 17:28:00 +0300

    added new socket event

--- frontend/src/components/SocketProvider.jsx
+++ frontend/src/components/SocketProvider.jsx
@@ -3,18 +3,27 @@ import { io } from 'socket.io-client';
 import PropTypes from 'prop-types';
 import SocketContext from '../contexts/index';
 import store from '../slices';
-import { actions } from '../slices/messagesSlice';
+import { actions as messagesActions } from '../slices/messagesSlice';
+import { actions as channelsActions } from '../slices/channelsSlice';
 
 const SocketProvider = ({ children }) => {
   const socket = io();
 
   socket.on('newMessage', (msg) => {
-    store.dispatch(actions.addMessage(msg));
+    store.dispatch(messagesActions.addMessage(msg));
+  });
+
+  socket.on('newChannel', (channel) => {
+    store.dispatch(channelsActions.addChannel(channel));
   });
 
   const sendMessage = (body, id) => socket.emit('newMessage', { body, channelId: id });
 
-  const value = useMemo(() => ({ sendMessage }), []);
+  const addChannel = (name) => socket.emit('newChannel', { name }, ({ data }) => {
+    store.dispatch(channelsActions.setCurrentChannelId(data.id));
+  });
+
+  const value = useMemo(() => ({ sendMessage, addChannel }), []);
 
   return (
     <SocketContext.Provider value={value}>

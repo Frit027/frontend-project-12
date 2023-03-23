75ccdfbf234c611f4902c09a55bca3ab107ea20b
Frit027 <Frit_027@mail.ru>
2023-03-23 11:34:28 +0300

    function for rendering modal

--- frontend/src/components/chat/Channels.jsx
+++ frontend/src/components/chat/Channels.jsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Button, ButtonGroup, Dropdown } from 'react-bootstrap';
-import { selectors, actions as channelsActions } from '../slices/channelsSlice';
-import AddChannel from './AddChannel';
+import { selectors, actions as channelsActions } from '../../slices/channelsSlice';
+import getModal from '../modals';
 
 const Channels = () => {
   const dispatch = useDispatch();
   const channels = useSelector(selectors.selectAll);
   const currentChannelId = useSelector((state) => state.channels.currentChannelId);
+
+  const [typeModal, setTypeModal] = useState(null);
+  const hideModal = () => setTypeModal(null);
+  const showModal = (type) => setTypeModal(type);
 
   const renderButton = (channel) => (
     <Button
@@ -31,9 +35,34 @@ const Channels = () => {
     </Dropdown>
   );
 
+  const renderModal = () => {
+    if (!typeModal) {
+      return null;
+    }
+    const Component = getModal(typeModal);
+    return <Component hideModal={hideModal} />;
+  };
+
   return (
     <>
-      <AddChannel />
+      <div className="d-flex mt-1 justify-content-between mb-2 ps-4 pe-2 p-4">
+        <b>Каналы</b>
+        <button
+          className="p-0 text-primary btn btn-group-vertical"
+          onClick={() => showModal('adding')}
+          type="button"
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="20" height="20" fill="currentColor">
+            <path
+              d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2
+                2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"
+            />
+            <path
+              d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"
+            />
+          </svg>
+        </button>
+      </div>
 
       <ul className="nav flex-column nav-pills nav-fill px-2 mb-3 overflow-auto h-100 d-block">
         {channels.map((channel) => (
@@ -42,6 +71,8 @@ const Channels = () => {
           </li>
         ))}
       </ul>
+
+      {renderModal()}
     </>
   );
 };

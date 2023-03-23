d3e65c60acd9c3151f48bb6f20209c5db37ddd8e
Frit027 <Frit_027@mail.ru>
2023-03-23 15:20:14 +0300

    added modal for renaming

--- frontend/src/components/modals/index.js
+++ frontend/src/components/modals/index.js
@@ -1,9 +1,11 @@
 import AddChannel from './AddChannel';
 import RemoveChannel from './RemoveChannel';
+import RenameChannel from './RenameChannel';
 
 const modals = {
   adding: AddChannel,
   removing: RemoveChannel,
+  renaming: RenameChannel,
 };
 
 export default (modalName) => modals[modalName];

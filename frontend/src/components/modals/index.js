7edeb67a592092ccd3c4c79117389891c240776e
Frit027 <Frit_027@mail.ru>
2023-03-23 14:01:58 +0300

    added modal for removing

--- frontend/src/components/modals/index.js
+++ frontend/src/components/modals/index.js
@@ -1,7 +1,9 @@
 import AddChannel from './AddChannel';
+import RemoveChannel from './RemoveChannel';
 
 const modals = {
   adding: AddChannel,
+  removing: RemoveChannel,
 };
 
 export default (modalName) => modals[modalName];

51c6326e458668be35aed55c0545202e90c260a9
Frit027 <Frit_027@mail.ru>
2023-03-23 16:21:21 +0300

    form tag replaced with component

--- frontend/src/components/modals/AddChannel.jsx
+++ frontend/src/components/modals/AddChannel.jsx
@@ -1,37 +1,15 @@
-import React, { useContext, useEffect, useRef } from 'react';
-import { Modal, Form, Button } from 'react-bootstrap';
-import { useFormik } from 'formik';
-import * as yup from 'yup';
-import { useSelector } from 'react-redux';
+import React, { useContext } from 'react';
+import { Modal } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import SocketContext from '../../contexts';
-import { selectors } from '../../slices/channelsSlice';
+import ChannelNameForm from '../forms/ChannelNameForm';
 
 const AddChannel = ({ hideModal }) => {
-  const input = useRef();
   const { addNewChannel } = useContext(SocketContext);
-  const channelNames = useSelector(selectors.selectAll).map((channel) => channel.name);
 
-  useEffect(() => {
-    input.current.focus();
-  }, []);
-
-  const formik = useFormik({
-    initialValues: {
-      name: '',
-    },
-    validationSchema: yup.object({
-      name: yup.string()
-        .min(3, 'От 3 до 20 символов')
-        .max(20, 'От 3 до 20 символов')
-        .notOneOf(channelNames, 'Должно быть уникальным')
-        .required('Обязательное поле'),
-    }),
-    onSubmit: ({ name }) => {
-      addNewChannel(name);
-      hideModal();
-    },
-  });
+  const handleSubmit = (name) => {
+    addNewChannel(name);
+  };
 
   return (
     <Modal show centered onHide={hideModal}>
@@ -39,27 +17,7 @@ const AddChannel = ({ hideModal }) => {
         <Modal.Title>Добавить канал</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-        <form onSubmit={formik.handleSubmit}>
-          <Form.Group>
-            <Form.Label class="visually-hidden">Имя канала</Form.Label>
-            <Form.Control
-              className="mb-2"
-              id="name"
-              name="name"
-              ref={input}
-              onChange={formik.handleChange}
-              value={formik.values.body}
-              autocomplete="off"
-              isInvalid={formik.touched.name && formik.errors.name}
-            />
-            <Form.Control.Feedback type="invalid">{formik.errors.name}</Form.Control.Feedback>
-          </Form.Group>
-
-          <div className="d-flex justify-content-end">
-            <Button className="me-2" variant="secondary" onClick={hideModal}>Отменить</Button>
-            <Button variant="primary" type="submit">Отправить</Button>
-          </div>
-        </form>
+        <ChannelNameForm handleSubmit={handleSubmit} hideModal={hideModal} />
       </Modal.Body>
     </Modal>
   );

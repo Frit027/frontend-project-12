6fc60545cf256cd0a5be4ea21ad471a994a2a5f0
Frit027 <Frit_027@mail.ru>
2023-03-30 15:07:04 +0300

    added route for signup page

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
+import SignUp from './pages/SignUp';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 import Header from './elements/Header';
@@ -12,6 +13,7 @@ const App = () => (
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="login" element={<Login />} />
+        <Route path="signup" element={<SignUp />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>

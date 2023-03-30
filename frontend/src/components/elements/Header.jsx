8ddcc724fc1e175dc1f08e4667a79630de311a2d
Frit027 <Frit_027@mail.ru>
2023-03-30 15:06:07 +0300

    header for click 'Logout'

--- frontend/src/components/elements/Header.jsx
+++ frontend/src/components/elements/Header.jsx
@@ -4,11 +4,16 @@ import { Container, Navbar, Button } from 'react-bootstrap';
 const Header = () => {
   const token = localStorage.getItem('token');
 
+  const logout = () => {
+    localStorage.removeItem('token');
+    window.location.href = '/login';
+  };
+
   return (
     <Navbar className="shadow-sm" expand="lg" variant="light" bg="white">
       <Container>
         <Navbar.Brand href="/">Hexlet Chat</Navbar.Brand>
-        {token ? <Button type="button" variant="primary">Выйти</Button> : null}
+        {token ? <Button type="button" variant="primary" onClick={logout}>Выйти</Button> : null}
       </Container>
     </Navbar>
   );

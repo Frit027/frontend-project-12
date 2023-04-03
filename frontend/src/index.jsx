98f5a0b825fa3a3a4e925deac36e36c548f04597
Frit027 <Frit_027@mail.ru>
2023-04-03 19:11:30 +0300

    render root moved to init file

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,18 +1,4 @@
-import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React from 'react';
-import { Provider } from 'react-redux';
-import App from './components/App';
-import store from './slices/index';
-import SocketProvider from './components/providers/SocketProvider';
+import runApp from './init';
 
-const root = ReactDOM.createRoot(document.getElementById('chat'));
-root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <SocketProvider>
-        <App />
-      </SocketProvider>
-    </Provider>
-  </React.StrictMode>,
-);
+runApp();

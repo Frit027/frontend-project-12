a258ac9eb5c0734f9b7bec187644236fedac511d
Frit027 <Frit_027@mail.ru>
2023-04-03 19:13:52 +0300

    added i18next and image

--- frontend/src/components/pages/NotFound.jsx
+++ frontend/src/components/pages/NotFound.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Image } from 'react-bootstrap';
+import { useTranslation } from 'react-i18next';
+import NotFoundImage from '../../images/not-found.svg';
 
-const NotFound = () => (
-  <div className="text-center">
-    <h1 className="h4 text-muted">Страница не найдена</h1>
-    <p className="text-muted">
-      Но вы можете перейти
-      <Link to="login"> на главную страницу</Link>
-    </p>
-  </div>
-);
+const NotFound = () => {
+  const { t } = useTranslation();
 
+  return (
+    <div className="text-center">
+      <Image className="h-25" src={NotFoundImage} alt={t('titles.notFound')} fluid />
+      <h1 className="h4 text-muted">{t('titles.notFound')}</h1>
+      <p className="text-muted">
+        {t('sentences.canRedirect')}
+        <Link to="login">{t('links.toHome')}</Link>
+      </p>
+    </div>
+  );
+};
 export default NotFound;

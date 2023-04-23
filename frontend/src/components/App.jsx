7a57f3d7583ec98b5dbadd281ccbe4c58a042604
Frit027 <Frit_027@mail.ru>
2023-04-23 17:42:28 +0300

    Header moved into Routes

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -8,8 +8,8 @@ import Header from './elements/Header';
 
 const App = () => (
   <div className="d-flex flex-column h-100">
-    <Header />
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="login" element={<Login />} />

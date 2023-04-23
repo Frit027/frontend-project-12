1a3be4968fc30a993d8296aac69fe6dd5b4294b7
Frit027 <Frit_027@mail.ru>
2023-04-23 17:42:51 +0300

    changed logout function

--- frontend/src/components/elements/Header.jsx
+++ frontend/src/components/elements/Header.jsx
@@ -1,21 +1,23 @@
 import React from 'react';
-import { Container, Navbar, Button } from 'react-bootstrap';
+import { Button, Container, Navbar } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
-  const { t } = useTranslation();
   const token = localStorage.getItem('token');
+  const navigate = useNavigate();
+  const { t } = useTranslation();
 
-  const logout = () => {
+  const logOut = () => {
     localStorage.removeItem('token');
-    window.location.href = '/login';
+    navigate('/login');
   };
 
   return (
     <Navbar className="shadow-sm" expand="lg" variant="light" bg="white">
       <Container>
         <Navbar.Brand href="/">{t('links.home')}</Navbar.Brand>
-        {token ? <Button type="button" variant="primary" onClick={logout}>{t('actions.logout')}</Button> : null}
+        {token ? <Button type="button" variant="primary" onClick={logOut}>{t('actions.logout')}</Button> : null}
       </Container>
     </Navbar>
   );

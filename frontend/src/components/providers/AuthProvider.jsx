55abe20ac3d0b1d79203c65d65108b31fd344516
Frit027 <Frit_027@mail.ru>
2025-10-29 21:41:36 +0300

    indentation set to 4 spaces

--- frontend/src/components/providers/AuthProvider.jsx
+++ frontend/src/components/providers/AuthProvider.jsx
@@ -4,33 +4,33 @@ import PropTypes from 'prop-types';
 export const AuthContext = createContext({});
 
 const AuthProvider = ({ children }) => {
-  const logIn = (data) => {
-    localStorage.setItem('userData', JSON.stringify({ token: data.token, username: data.username }));
-  };
+    const logIn = (data) => {
+        localStorage.setItem('userData', JSON.stringify({ token: data.token, username: data.username }));
+    };
 
-  const logOut = () => {
-    localStorage.removeItem('userData');
-  };
+    const logOut = () => {
+        localStorage.removeItem('userData');
+    };
 
-  const isLoggedIn = () => !!localStorage.getItem('userData');
+    const isLoggedIn = () => !!localStorage.getItem('userData');
 
-  const getToken = () => JSON.parse(localStorage.getItem('userData')).token;
+    const getToken = () => JSON.parse(localStorage.getItem('userData')).token;
 
-  const getUsername = () => JSON.parse(localStorage.getItem('userData')).username;
+    const getUsername = () => JSON.parse(localStorage.getItem('userData')).username;
 
-  const value = useMemo(() => ({
-    logIn, logOut, isLoggedIn, getToken, getUsername,
-  }), [logIn, logOut, isLoggedIn, getToken, getUsername]);
+    const value = useMemo(() => ({
+        logIn, logOut, isLoggedIn, getToken, getUsername,
+    }), [logIn, logOut, isLoggedIn, getToken, getUsername]);
 
-  return (
-    <AuthContext.Provider value={value}>
-      {children}
-    </AuthContext.Provider>
-  );
+    return (
+        <AuthContext.Provider value={value}>
+            {children}
+        </AuthContext.Provider>
+    );
 };
 
 AuthProvider.propTypes = {
-  children: PropTypes.node.isRequired,
+    children: PropTypes.node.isRequired,
 };
 
 export default AuthProvider;
--- frontend/src/components/providers/SocketProvider.jsx
+++ frontend/src/components/providers/SocketProvider.jsx
@@ -7,36 +7,36 @@ import { actions as channelsActions } from '../../slices/channelsSlice';
 export const SocketContext = createContext({});
 
 const SocketProvider = ({ children, socket }) => {
-  const addNewMessage = (body, username, channelId, resolve) => {
-    socket.emit('newMessage', { body, username, channelId }, (response) => {
-      if (response.status === 'ok') {
-        resolve();
-      }
+    const addNewMessage = (body, username, channelId, resolve) => {
+        socket.emit('newMessage', { body, username, channelId }, (response) => {
+            if (response.status === 'ok') {
+                resolve();
+            }
+        });
+    };
+
+    const addNewChannel = (name) => socket.emit('newChannel', { name }, ({ data }) => {
+        store.dispatch(channelsActions.setCurrentChannelId(data.id));
     });
-  };
 
-  const addNewChannel = (name) => socket.emit('newChannel', { name }, ({ data }) => {
-    store.dispatch(channelsActions.setCurrentChannelId(data.id));
-  });
+    const removeChannel = (id) => socket.emit('removeChannel', { id });
 
-  const removeChannel = (id) => socket.emit('removeChannel', { id });
+    const renameChannel = (id, name) => socket.emit('renameChannel', { id, name });
 
-  const renameChannel = (id, name) => socket.emit('renameChannel', { id, name });
+    const value = useMemo(() => ({
+        addNewMessage, addNewChannel, removeChannel, renameChannel,
+    }), [addNewMessage, addNewChannel, removeChannel, renameChannel]);
 
-  const value = useMemo(() => ({
-    addNewMessage, addNewChannel, removeChannel, renameChannel,
-  }), [addNewMessage, addNewChannel, removeChannel, renameChannel]);
-
-  return (
-    <SocketContext.Provider value={value}>
-      {children}
-    </SocketContext.Provider>
-  );
+    return (
+        <SocketContext.Provider value={value}>
+            {children}
+        </SocketContext.Provider>
+    );
 };
 
 SocketProvider.propTypes = {
-  children: PropTypes.node.isRequired,
-  socket: PropTypes.instanceOf(Socket).isRequired,
+    children: PropTypes.node.isRequired,
+    socket: PropTypes.instanceOf(Socket).isRequired,
 };
 
 export default SocketProvider;
